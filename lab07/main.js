1969e03e790ef1a8712cc50e9e2ef65a638b585b
Lena <helly.luchina@gmail.com>
2021-04-20 12:44:15 +0300

    input output

--- lab07/main.js
+++ lab07/main.js
@@ -2,8 +2,8 @@
 const array_sum = (accumulator, currentValue) => accumulator + currentValue
 const array_mult = (accumulator, currentValue) => accumulator * currentValue
 
-const languages = [["SQL", 30], ["JavaScript", 56], ["Java", 54], ["ASM", 320], ["C", 128], ["C++", 53]]
-
+const languages = {"C++":53}
+let number_of_languages = 1
 
 class Model {
 	constructor() {
@@ -84,6 +84,24 @@ class Model {
 		this.Flexibility = parseFloat(document.getElementById("Flexibility").value)
 
 		this.Fi_values = [this.Data_exchange, this.Distributed_processing, this.Performance, this.Operating_Limitations_on_Hardware_Resources, this.Transactional_Load, this.Intensity_of_User_Interaction, this.Ergonomics_Affecting_End_User_Efficiency, this.Online_update, this.Processing_complexity, this.Reuse, this.Ease_of_installation, this.Ease_of_use_administration, this.Portability, this.Flexibility]
+
+		this.languages = []
+		var allprop = 0
+		for (let i = 0; i < number_of_languages; i++) {
+			var id = "lang" + i
+			var lan_fp =  parseFloat(document.getElementById(id).value)
+			var prop = parseFloat(document.getElementById(id + "prop").value)
+			allprop += prop
+			console.log(id, prop, lan_fp)
+			if (lan_fp == undefined)
+				alert("unknown programming language")
+			this.languages.push({
+				prop: prop,
+				fp: lan_fp
+			})
+		}
+		if (allprop != 100)
+			alert("not 100% of code covered with programming language")
 	}
 	insert_Cocomo_data() {
 		this.RCPX = parseFloat(document.getElementById("RCPX").value)
@@ -94,6 +112,7 @@ class Model {
 		this.FCIL = parseFloat(document.getElementById("FCIL").value)
 		this.SCED = parseFloat(document.getElementById("SCED").value)
 		this.Cocomo_values = [this.RCPX, this.RUSE, this.PERS, this.PREX, this.PDIF, this.FCIL, this.SCED]
+		this.payment = parseFloat(document.getElementById("payment").value)
 	}
 	insert_p_data() {
 		this.PREC = parseFloat(document.getElementById("PREC").value)
@@ -109,7 +128,12 @@ class Model {
 		// 30% кода будет написано на SQL (13 LOC на один оператор);
 		// 10 % - на JavaScript (56 LOC);
 		// 60% - на Java (53 LOC).
-		this.kloc = (0.3 * 13 + 0.1 * 56 + 0.6 * 53) * this.fp / 1000
+		this.kloc = 0
+		for (let i = 0; i < this.languages.length; i++)
+		{
+			this.kloc += this.languages[i].prop / 100 * this.languages[i].fp
+		}
+		this.kloc *= this.fp / 1000
 
 		return this.kloc
 	}
@@ -118,18 +142,40 @@ class Model {
 		this.work = 2.45 * this.Cocomo_values.reduce(array_mult) * Math.pow(this.kloc, this.p)
 		this.time = 3.0 * Math.pow(this.work, 0.33 + 0.2 * (this.p - 1.01))
 		this.workers = Math.round(this.work / this.time)
-		this.budget = this.workers * 150000 * this.time
+		this.budget = this.workers * this.payment * this.time
 	}
 }
 
+var dict_lang = {
+	"assembley": 320,
+	"c": 128,
+	"kobol": 196,
+	"fortran": 106,
+	"pascal": 90,
+	"c++": 53,
+	"java; c#": 53,
+	"sql": 13,
+	"js": 56,
+	"ada 95": 49,
+	"visual basic": 24,
+	"visual c++": 34,
+	"delphi": 29,
+	"perl": 21,
+	"prolog": 54,
+	};
+
 function generate() {
-	var model = new Model()
-	for (i = 0; i < model.p_factors_labels.length; i++)
+	console.log("hello")
+	console.log(Object.keys(dict_lang).length)
+	console.log(dict_lang[Object.keys(dict_lang)[1]])
+	console.log(Object.keys(dict_lang)[1])
+	for (i = 0; i < Object.keys(dict_lang).length; i++)
 	{
-		id = model.p_factors_labels[i]
-		name = model.p_factors_labels_meanings[i]
-		v = model.p_factors_possible_values[i]
-		console.log(`this.${id} = parseFloat(document.getElementById("${id}").value)`)
+		console.log(`<option value="${dict_lang[Object.keys(dict_lang)[i]]}">${Object.keys(dict_lang)[i]}</option>`)
+		// id = model.p_factors_labels[i]
+		// name = model.p_factors_labels_meanings[i]
+		// v = model.p_factors_possible_values[i]
+		// console.log(`this.${id} = parseFloat(document.getElementById("${id}").value)`)
 	// 	console.log(
 	// `	<div inputdata>
 	// 		<label for="${id}">${name} (${id})</label>
@@ -193,6 +239,9 @@ function setdata() {
 	model.calcukate_KLOC(79)
 	model.calculate_Cocomo()
 
+
+	var outputstr = `количество функциональных точек ${model.fp}<br>размер кода ${model.kloc} kloc<br>показатель степени${model.p}<br>
+	трудозатраты ${model.work} человеко-месяцев<br>время ${model.time} месяцев<br>количество работников ${model.workers}<br>бюджет ${model.budget}`
 	console.log("fp", model.fp)
 	console.log("kloc", model.kloc)
 	console.log("p", model.p)
@@ -200,4 +249,40 @@ function setdata() {
 	console.log("time", model.time)
 	console.log("people", model.workers)
 	console.log("budget", model.budget)
+
+	var body = document.body
+	var output = document.createElement('p');
+	output.innerHTML = outputstr
+	body.appendChild(output);
+}
+
+function addlang()
+{
+
+	var landiv = document.getElementById("langs")
+	var newlanguage = document.createElement('div');
+	newlanguage.innerHTML = `<div inputdata>
+	<label for="lang${number_of_languages}">яп: </label>
+	<select name="lang${number_of_languages}" id="lang${number_of_languages}">
+		<option value="320">assembley</option>
+		<option value="128">c</option>
+		<option value="196">kobol</option>
+		<option value="106">fortran</option>
+		<option value="90">pascal</option>
+		<option value="53">c++</option>
+		<option value="53">java; c#</option>
+		<option value="13">sql</option>
+		<option value="56">js</option>
+		<option value="49">ada 95</option>
+		<option value="24">visual basic</option>
+		<option value="34">visual c++</option>
+		<option value="29">delphi</option>
+		<option value="21">perl</option>
+		<option value="54">prolog</option>
+	</select>
+	<input id="lang${number_of_languages}prop" value="0">
+	<label for="lang${number_of_languages}prop">%</label>
+	</div>`
+	landiv.appendChild(newlanguage);
+	number_of_languages += 1
 }
